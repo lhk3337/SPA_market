8156970bc410ee564c37a3c5d79bc95f6e74b605
lhk3337 <lhk3337@naver.com>
2022-04-11 21:13:04 +0900

    [FEAT] 모달페이지 바로구매 버튼 및 품절 상태에서 style설정 #15

--- src/js/components/ProductList/ProductModal.js
+++ src/js/components/ProductList/ProductModal.js
@@ -44,40 +44,56 @@ export default function ProductModal({ $target, productId, listRender }) {
                 <div class="price">
                   <span class="price__title">${price.toLocaleString("ko-KR")}</span><span>원</span>         
                 </div>
-                <p class="shipFee">택배배송 / ${
-                  shippingFee === 0 ? "무료배송" : `${shippingFee.toLocaleString("ko-KR")}원`
-                }
-                </p>
-                <div class="count__box">
-                  <div class="count__countainer">
-                    <button id="minus__btn">
-                      <img class="minus" src="/src/assets/icon-minus-line.svg" />
-                    </button>
-                    <input type="number" class="input__count" min="1" max="${stockCount}" value="${count}" />
-                    <button id="plus__btn">
-                      <img class="plus" src="/src/assets/icon-plus-line.svg" />
-                    </button>
+                <div class="${stockCount > 0 ? `` : `not__count__info`}">
+                  <p class="shipFee">택배배송 / ${
+                    shippingFee === 0 ? "무료배송" : `${shippingFee.toLocaleString("ko-KR")}원`
+                  }
+                  </p>
+                  <div class="count__box">
+                    <div class="count__countainer">
+                      <button id="minus__btn">
+                        <img class="minus" src="/src/assets/icon-minus-line.svg" />
+                      </button>
+                      <input type="number" class="input__count" min="1" max="${stockCount}" value="${count}" />
+                      <button id="plus__btn">
+                        <img class="plus" src="/src/assets/icon-plus-line.svg" />
+                      </button>
+                    </div>
+                  </div>
+                  <div class="total__price__container">
+                    <h3>총 상품 금액</h3>
+                    <div class="total__price">
+                      <span class="price__text">총 수량 <span class="count">${count}</span>개</span>
+                      <div class="border"></div>
+                      <h2 class="total__price__title">${(count * price).toLocaleString(
+                        "ko-KR"
+                      )}<span class="total__price__text">원</span></h2>
+                    </div>
                   </div>
                 </div>
-                <div class="total__price__container">
-                  <h3>총 상품 금액</h3>
-                  <div class="total__price">
-                    <span class="price__text">총 수량 <span class="count">${count}</span>개</span>
-                    <div class="border"></div>
-                    <h2 class="total__price__title">${(count * price).toLocaleString(
-                      "ko-KR"
-                    )}<span class="total__price__text">원</span></h2>
-                  </div>
-                </div>
-                <div class="like__detail__item">
+                <div class="btn__container ${stockCount > 0 ? `` : `margin`}">
                 ${
-                  getLiked === null
-                    ? `<img class="like__detail__off" src="/src/assets/icon-heart.svg">`
-                    : `<img class="like__detail__on" src="/src/assets/icon-heart-on.svg">`
+                  stockCount > 0
+                    ? `
+                  <button class="buy__btn">바로 구매</button>
+                  <div class="cart__detail__item"><img src="/src/assets/icon-shopping-cart.svg" /></div>
+                `
+                    : `
+                  <button class="buy__btn   cancel">품절된 상품입니다.</button>
+                  <div class="cart__detail__item cancel"><img src="/src/assets/icon-shopping-cart-white.svg" /></div>
+                `
                 }
+                  <div class="like__detail__item">
+                  ${
+                    getLiked === null
+                      ? `<img class="like__detail__off" src="/src/assets/icon-heart.svg">`
+                      : `<img class="like__detail__on" src="/src/assets/icon-heart-on.svg">`
+                  }
+                  </div>
                 </div>
               </div>
-            <div>
+            </div>
+          <div class="product__info">sss</div>
         </div>
     `;
     const $closeBtn = document.querySelector(".closeBtn");
