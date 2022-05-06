4977cc79b3e4ed9a2dc642d3dba86358ce3e0679
lhk3337 <lhk3337@naver.com>
2022-05-06 13:46:14 +0900

    [FIX] - modal close 버튼 위치 수정

--- src/js/components/ProductList/ProductModal/ProductModal.js
+++ src/js/components/ProductList/ProductModal/ProductModal.js
@@ -33,24 +33,26 @@ export default function ProductModal({ $target, productId, listRender }) {
     $modal.innerHTML = `
         <div class="product__content">
           <button class="closeBtn"><img src="/src/assets/icon-delete.svg" /></button>
-            <div class="container">
+          <div class="container">
+            <div class="info__container">
               <div class="left__container">
                 <img class="thumbnailImg" src="http://test.api.weniv.co.kr/${thumbnailImg}" />
               </div>
             </div>
-          <div class="product__info">
-            <h1 class="info__title">상품 정보</h1>
-            <table>
-              <tr>
-                <td class="title">상품 번호</td>
-                <td class="content">${pubDate.replace(/\-/g, "")}</td>
-                <td class="title">재고 수량</td>
-                <td class="content">${stockCount > 0 ? `${stockCount}개` : "-"}</td>
-              </tr>
-            </table>
-          </div>
-          <div class="detail__page">
-            ${detailInfoImage.map((image) => `<img src="http://test.api.weniv.co.kr/${image}" />`).join("")}
+            <div class="product__info">
+              <h1 class="info__title">상품 정보</h1>
+              <table>
+                <tr>
+                  <td class="title">상품 번호</td>
+                  <td class="content">${pubDate.replace(/\-/g, "")}</td>
+                  <td class="title">재고 수량</td>
+                  <td class="content">${stockCount > 0 ? `${stockCount}개` : "-"}</td>
+                </tr>
+              </table>
+            </div>
+            <div class="detail__page">
+              ${detailInfoImage.map((image) => `<img src="http://test.api.weniv.co.kr/${image}" />`).join("")}
+            </div>
           </div>
         </div>
     `;
@@ -63,7 +65,7 @@ export default function ProductModal({ $target, productId, listRender }) {
 
     console.log(this.state);
     new ModalOrder({
-      $target: $modal.querySelector(".container"),
+      $target: $modal.querySelector(".info__container"),
       data: {
         product: this.state.product,
         count: this.state.count,
