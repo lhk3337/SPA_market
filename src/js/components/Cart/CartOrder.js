73af285871f928f1b5d4bc2a6321e878aa83b507
lhk3337 <lhk3337@naver.com>
2022-05-13 21:26:04 +0900

    [STYLE] cart page - 카트 상품 리스트 글씨 스타일 설정

--- src/js/components/Cart/CartOrder.js
+++ src/js/components/Cart/CartOrder.js
@@ -34,17 +34,19 @@ export default function OrderProduct({ products, $page, cartRender }) {
       <label for="listCheck${product.productId}"></label>
       <img class="cart__thumbnail" src="http://test.api.weniv.co.kr/${product.image}" />
       <div class="cart__product__info">
-        <p>${product.productName}</p>
-        <h3>${product.productPrice}</h3>
-        <p>${product.optionName === null ? `수량 ${product.qty}개` : `${product.optionName} ${product.qty}개`}  </p>
+        <p>${product.productName.length < 25 ? product.productName : product.productName.substring(0, 20) + "..."}</p>
+        <h3>${product.productPrice}원</h3>
+        <p class="count">${
+          product.optionName === null ? `수량 ${product.qty}개` : `${product.optionName} ${product.qty}개`
+        }  </p>
       </div>
       <div class="cart__right__container">
         <div class="coupon__discount">
           <p>Hack Your Life 개발자 노트북...</p>
           <h3>-2000원</h3>
         </div>
-        <p>${product.shippingFee}</p>
-        <h3 class="cart__price">${product.productPrice + product.shippingFee}</h3>
+        <p class="cart__shipFee">${product.shippingFee > 0 ? `${product.shippingFee}원` : "무료배송"}</p>
+        <h3 class="cart__price">${product.productPrice + product.shippingFee}원</h3>
       </div>
     </li>
     `
